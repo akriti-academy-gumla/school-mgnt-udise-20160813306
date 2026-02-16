d3a919e273f424f546b4e7d86ad5e05c6c918b8a
nitesh.dev <nitesh.dev@proton.me>
2026-02-16 16:27:52 +0530

    chore: run hello world

--- server/index.js
+++ server/index.js
@@ -5,17 +5,21 @@ import { createAdmin } from "./controllers/admin.ctrl.js";
 import cookieParser from "cookie-parser";
 import adminRoutes from "./routes/adminAuth.routes.js";
 import studentAuthRoutes from "./routes/studentAuth.routes.js";
+import path from "node:path";
+import { fileURLToPath } from "url";
 
 // middleware
 import adminAuthorizationMiddleware from "./middlewares/authorization.mid.js"
 import studentRoutes from "./routes/student.routes.js";
 // const deafultMongoURL = "mongodb://localhost:27017/";
 console.clear()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const server = express();
 
 server.use(express.json());
 server.use(cookieParser())
-
+server.use(express.static(path.join(__dirname, "public")));
 
 server.use("/api/v1/admin", adminRoutes);
 server.use("/api/v1/", studentAuthRoutes);
